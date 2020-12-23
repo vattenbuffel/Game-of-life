3824e0c7ec332069a4b60a77ba7e0b2daf1dbb2e
Noa <noal@student.chalmers.se>
2020-12-23 14:52:39 +0100

    Add scroll functionality

--- game-of-life.py
+++ game-of-life.py
@@ -85,30 +85,41 @@ class GUI:
         self.max_height = 480
         self.cell_height = 500
         self.cell_width = 500
-        self.cell_margin = 10
+        self.cell_margin = 0
         self.grid_border_width = 1# % of the cell_width
         self.bg_color = "#434547"
         self.grid_color = "white"
         self.fps = 60
+        self.cell_height = 25
+        self.cell_width = 25
 
         self.n_rows = n_rows
         self.n_cols = n_cols
         self.run_game = False
         self.step = False
+        self.draw_board = False # Signals if the board should be drawn
 
         # Calculate the height and width of the cells
         height = n_rows*self.cell_height + 2*self.cell_margin
         width = n_rows*self.cell_width + 2*self.cell_margin
-        if height > self.max_height:
-            height = self.max_height
-            self.cell_height  = (self.max_height-2*self.cell_margin)/self.n_rows
-            
-        if width > self.max_width:
-            width = self.max_width
-            self.cell_width  = (self.max_width-2*self.cell_margin)/self.n_cols
-
         self.grid_border_width *= self.cell_width/100
 
+        # How many cells should be visable
+        if height < self.max_height:
+            self.n_rows_visable = self.n_rows
+        else:
+            self.n_rows_visable = int(self.max_height/self.cell_height)
+            height = self.max_height
+        
+        if width < self.max_width:
+            self.n_cols_visable = self.n_cols
+        else:
+            self.n_cols_visable = int(self.max_width/self.cell_width)
+            width = self.max_width
+        self.top_left_cell_x = 0
+        self.top_left_cell_y = 0
+            
+        
         self.game = Game(n_cols, n_rows)
         self.top = tk.Tk()
         
@@ -116,21 +127,21 @@ class GUI:
 
         # Add canvas
         self.top.rowconfigure(0, minsize=25, weight=2)
-        self.top.columnconfigure(0, minsize=25, weight=2)
+        self.top.columnconfigure(1, minsize=25, weight=2)
         self.frame_canvas = tk.Frame(self.top)
         self.canvas = tk.Canvas(self.frame_canvas, bg=self.bg_color, height=height, width=width)
-        self.frame_canvas.grid(row=0, column=0)
+        self.frame_canvas.grid(row=0, column=1)
         self.canvas.bind("<Button-1>", self.cell_clicked)
         self.canvas.pack(fill=tk.BOTH)
 
 
         # Add buttons
         self.top.rowconfigure(0, minsize=height, weight=1)
-        self.top.columnconfigure(1, minsize=75, weight=1)
+        self.top.columnconfigure(0, minsize=75, weight=1)
         self.frame_buttons = tk.Frame(self.top)
         
         self.button_start_stop = tk.Button(self.frame_buttons, text="Start", command = lambda : self.start_button_clicked())
-        self.frame_buttons.grid(row=0, column=1, sticky="n", padx=5, pady=5)
+        self.frame_buttons.grid(row=0, column=0, sticky="n", padx=5, pady=5)
         self.button_start_stop.pack()
         
         self.button_step = tk.Button(self.frame_buttons, text="Step", command = lambda : self.button_step_clicked())
@@ -147,9 +158,31 @@ class GUI:
         self.button_save = tk.Button(self.frame_buttons, text="Save As...", command=self.save_file)
         self.button_save.pack()
 
+        # Add sliders to move cells left and right and up and down
+        self.frame_slider_row = tk.Frame(self.top)
+        self.frame_slider_row.grid(row=0, column=3, padx=5)
+        self.slider_row = tk.Scale(self.frame_slider_row, length = height, from_=0, to=n_rows-self.n_rows_visable, tickinterval=0, command = lambda val : self.row_slider_update(val))
+        self.slider_row.set(self.top_left_cell_y)
+        self.slider_row.pack()
+
+        self.frame_slider_col = tk.Frame(self.top)
+        self.frame_slider_col.grid(row=1, column=1, padx=5)
+        self.slider_col = tk.Scale(self.frame_slider_col, length = width, from_=0, to=n_cols-self.n_cols_visable, tickinterval=0, command = lambda val : self.col_slider_update(val), orient=tk.HORIZONTAL)
+        self.slider_col.set(self.top_left_cell_x)
+        self.slider_col.pack()
+        
+        
+
         self.draw_grid()
         self.last_update = time.time()
         
+    def row_slider_update(self, val):
+        self.draw_board = True
+        self.top_left_cell_y = int(val)
+    
+    def col_slider_update(self, val):
+        self.draw_board = True
+        self.top_left_cell_x = int(val)
 
     def open_file(self):
         """Open a file for editing."""
@@ -180,9 +213,6 @@ class GUI:
                 index = [int(num) for num in re.findall(r'\d+', data)]
                 self.game.update_cell(index[0], index[1])
                 
-
-            
-
     def save_file(self):
         """Save the current file as a new file."""
         filepath = asksaveasfilename(
@@ -199,7 +229,6 @@ class GUI:
 
             output_file.write(game_config)
             output_file.write(board_txt)
-
 
     def slider_fps_change(self, val):
         self.fps = float(val)
@@ -238,26 +267,34 @@ class GUI:
         row = int((event.y-self.cell_margin)/self.cell_height)
         
         # Check for click outside of grid
-        if col < 0 or col > self.n_cols-1 or row < 0 or row > self.n_rows-1: return
+        if col < 0 or col > self.n_cols_visable-1 or row < 0 or row > self.n_rows_visable-1: return
+
+        # Calculate which cell was clicked
+        col += self.top_left_cell_x
+        row += self.top_left_cell_y
 
         self.game.cell_clicked(row, col)
 
     # Clear the board and redraw it. Only if something's been updated though
     def draw_updated(self):
-        if len(self.game.updated_cells) == 0: return
+        if len(self.game.updated_cells) == 0 and not self.draw_board: return
 
         gui.canvas.delete("all")
 
+        x_visible = range(self.top_left_cell_x, self.top_left_cell_x + self.n_cols_visable)
+        y_visible = range(self.top_left_cell_y, self.top_left_cell_y + self.n_rows_visable)
         for key in self.game.alive_cells:
-            self.fill_cell(key)
+            # Check if the cell is visible
+            if key[0] in y_visible and key[1] in x_visible:
+                location = (key[0] - self.top_left_cell_y, key[1] - self.top_left_cell_x)
+                self.fill_cell(location)
 
         self.game.updated_cells = {}
+        self.draw_board = False
 
         self.draw_grid()
-
-            
-    def update(self):
         
+    def update(self):
         if self.step:
             self.game.update_board()
             self.step = False
